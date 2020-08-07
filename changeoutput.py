f77d40f0984e875781d301212560747b136fa667
Jaydip <46409262+KillaBoi@users.noreply.github.com>
2020-08-07 04:29:09 +0100

    Update changeoutput.py

--- changeoutput.py
+++ changeoutput.py
@@ -1,5 +1,5 @@
 import os,sys
-folder = ('C:\\Users\\Admin\\AppData\\Roaming\\RGhPduxyvqotGwjLqAW\DHeIgPub\\walkbot\\data2\\')
+folder = ('DIR')
 for filename in os.listdir(folder):
        infilename = os.path.join(folder,filename)
        if not os.path.isfile(infilename): continue
